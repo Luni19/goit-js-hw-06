5ffa2376f6b53de852eb53f4a0b608ed66aa7960
Konstantin <aziimov1999123@gmail.com>
2023-08-14 21:29:55 +0300

    2

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -2,11 +2,11 @@ function countCategoriesAndItems() {
     const categoriesList = document.getElementById('categories');
     const categoriesItems = categoriesList.querySelectorAll('li.item');
 
-    // Кількість категорій (елементів li.item)
+   
     const numCategories = categoriesItems.length;
     console.log(`Кількість категорій: ${numCategories}`);
 
-    // Для кожної категорії виводимо текст заголовку (h2) та кількість елементів (li) в категорії
+   
     categoriesItems.forEach((category) => {
       const categoryName = category.querySelector('h2').textContent;
       const categoryItems = category.querySelectorAll('li').length;
@@ -14,5 +14,4 @@ function countCategoriesAndItems() {
     });
   }
 
-  // Викликаємо функцію для підрахунку та виведення результатів в консоль
   countCategoriesAndItems();
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulElement = document.querySelector("#ingredients");
+
+const fragment = document.createDocumentFragment();
+
+ingredients.forEach((ingredient) => {
+  const liElement = document.createElement("li")
+  
+  liElement.textContent = ingredient
+  
+  liElement.classList.add("item");
+  
+  fragment.appendChild(liElement);
+});
+
+
+ulElement.appendChild(fragment)
